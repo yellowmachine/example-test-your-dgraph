11e4a25d9b20053b5f0cfdde3f46ae2216bae684
yellow <yellow.machine@mailfence.com>
2022-11-11 12:51:50 +0100

    Show quit message

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const {context: C} = require("yellow-machine")
+const {context: C, SHOW_QUIT_MESSAGE} = require("yellow-machine")
 const npm = require('npm-commands')
 const {docker} = require('./docker')
 const {dgraph} = require('./dgraph')
 const config = require("./config")
 
+SHOW_QUIT_MESSAGE.v = true
+
 function test(){
     npm().run('tap');
 }
 
-const {up, down} = docker({name: "my-container-dgraph-v6", 
+const {up, down} = docker({name: "my-container-dgraph-v13", 
                            image: "dgraph/standalone:master", 
                            port: "8080"
                         })
